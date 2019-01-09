1307777724b134a83daccee0b5c81fddf28fd74d
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2019-01-09 12:01:31 +0100

    Added PropertyField attributes and methods.

--- src/main/java/PropertyField.java
+++ src/main/java/PropertyField.java
@@ -2,13 +2,45 @@ import java.awt.*;
 
 public class PropertyField extends Field {
 
-    public PropertyField(String title, String subtitle, String description, String message, Color fillColor){
+    private String type;
+    private int price;
+    private int buildingPrice;
+    private int buildingCount;
+    private int[] rents;
+
+    public PropertyField(String title, String subtitle, String description, String message, Color fillColor, String type, int price, int buildingPrice, int buildingCount, int... rents){
         super(title, subtitle, description, message, fillColor);
+
+        this.type = type;
+        this.price = price;
+        this.buildingPrice = buildingPrice;
+        this.buildingCount = buildingCount;
+        this.rents = rents;
     }
 
     @Override
     public void accept(Visitor visitor){
         visitor.visit(this);
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public int getBuildingPrice(){
+        return buildingPrice;
+    }
+
+    public int getBuildingCount(){
+        return buildingCount;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public int getRent(){
+        return rents[buildingCount];
     }
 
 }
