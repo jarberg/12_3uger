3ee73272a051f920d15f09fe9b661b4fd1fcfef4
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2019-01-09 16:30:21 +0100

    Created addPlayer method

--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -5,18 +5,33 @@ import gui_main.GUI;
 public class ViewController {
 
     private static final ViewController singleInstance = new ViewController();
-
     private GUI gui;
     private GUI_Field[] gui_board;
     private GUI_Player[] gui_players;
 
     private ViewController(){
+        this.gui_board = new GUI_Field[40];
     }
 
-    public ViewController getSingleInstance(){
+    public static ViewController getSingleInstance(){
         return singleInstance;
     }
 
+    public void showEmptyGUI(){
+        this.gui = new GUI(new GUI_Field[0]);
+    }
+
+    public void closeGUI(){
+        this.gui.close();
+    }
+
+    public void addPlayer(String name, int balance){
+        int length = gui_players.length;
+        GUI_Player[] temp = new GUI_Player[length+1];
+        System.arraycopy(gui_players, 0, temp, 0, length);
+        gui_players = temp;
+        gui_players[length] = new GUI_Player(name, balance);
+    }
 
 
 }
