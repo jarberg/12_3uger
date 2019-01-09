303bd298a5f8c614d64a8171a8ab228ac6317c91
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2019-01-09 17:16:23 +0100

    Added move player

--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -33,12 +33,42 @@ public class ViewController {
             length = 0;
         }
         GUI_Player[] temp = new GUI_Player[length+1];
-        //System.arraycopy(gui_players, 0, temp, 0, length);
+        for (int i = 0; i < length; i++) {
+            temp[i] = gui_players[i];
+        }
         gui_players = temp;
         gui_players[length] = new GUI_Player(name, balance);
     }
 
-    public GUI_Player[] getGui_players() {
-        return gui_players;
+    public void spawnPlayers(){
+        if(gui_board[0] != null){
+            for(GUI_Player player : gui_players){
+                gui_board[0].setCar(player, true);
+            }
+        }
+    }
+
+    public void movePlayer(String playerName, int position, int amount){
+        GUI_Player movingPlayer = getPlayerByName(playerName);
+        for (int i = 0; i < amount; i++) {
+            gui_board[position].setCar(movingPlayer, false);
+            position++;
+            position = position % gui_board.length;
+            gui_board[position].setCar(movingPlayer, true);
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private GUI_Player getPlayerByName(String playerName){
+        GUI_Player player = null;
+        for(GUI_Player p : gui_players){
+            if(p.getName().equals(playerName))
+                player = p;
+        }
+        return player;
     }
 }
