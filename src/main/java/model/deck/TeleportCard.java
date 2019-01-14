34b446b64d6c5968f0b8a28b927dc79307111198
Marina <marina@mallingnet.dk>
2019-01-14 11:51:20 +0100

    Ordnet TODO i teleportcard

--- src/main/java/model/deck/TeleportCard.java
+++ src/main/java/model/deck/TeleportCard.java
@@ -6,9 +6,10 @@ public class TeleportCard extends Card {
 
     private int position;
 
-    //TODO: Make sure parameter "int position" is used in constructor
     public TeleportCard(String description, int position) {
         super(description);
+
+        this.position = position;
     }
 
     public void accept (Drawer drawer) {
