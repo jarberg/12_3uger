dc13bceb23632aebd90b1b4968ebb9d85362e72b
Marina <marina@mallingnet.dk>
2019-01-14 11:47:31 +0100

    Ordnet TODO i teleport

--- src/main/java/model/deck/PayForBuildingsCard.java
+++ src/main/java/model/deck/PayForBuildingsCard.java
@@ -6,7 +6,6 @@ public class PayForBuildingsCard extends Card {
     private int house;
     private int hotel;
 
-    //make int house, int hotel into attributes and get methods
 
     public PayForBuildingsCard(String description, int house, int hotel) {
         super(description);
