e7ef4d18dc50f5ecaf13d2114ec3412bfa627988
Marina <marina@mallingnet.dk>
2019-01-18 13:02:28 +0100

    drawcontroller test

--- src/test/java/controller/DrawControllerTest.java
+++ src/test/java/controller/DrawControllerTest.java
@@ -2,9 +2,7 @@ package controller;
 
 import model.board.Board;
 import model.board.Field;
-import model.deck.Card;
-import model.deck.Deck;
-import model.deck.MonopolyJackpotCard;
+import model.deck.*;
 import model.player.Player;
 import org.junit.After;
 import org.junit.Before;
@@ -32,16 +30,16 @@ public class DrawControllerTest {
 
         player = gamecontroller.getPlayer(0);
 
-        for (int i = 1; i == viewcontroller.getPLayerAmount() ; i++) {
+        for (int i = 1; i == viewcontroller.getPLayerAmount(); i++) {
             otherplayerList[i] = gamecontroller.getPlayer(i);
         }
 
         tradecontroller.setViewController(viewcontroller);
 
-        otherplayerList = new Player[viewcontroller.getPLayerAmount()-1];
+        otherplayerList = new Player[viewcontroller.getPLayerAmount() - 1];
 
         gamecontroller.setupGame();
-        drawcontroller = new DrawController(player,otherplayerList,bank,gamecontroller.board,gamecontroller.deck,viewcontroller);
+        drawcontroller = new DrawController(player, otherplayerList, bank, gamecontroller.board, gamecontroller.deck, viewcontroller);
 
 
     }
@@ -52,52 +50,29 @@ public class DrawControllerTest {
     }
 
 
-   /*
     @Test
     public void GetOutOfJailCard() {
-        //teste om use af card virker
 
-        boolean playerInJail = true;
-        boolean getOutOfJailCard = false;
+        GetOutOfJailCard card = new GetOutOfJailCard("desc", 50);
 
-        if (getOutOfJailCard == true) {
-            playerInJail = false;
-            System.out.println( "player is not in jail");
-            assertFalse(playerInJail);
-        } else if (getOutOfJailCard == false) {
-            playerInJail = true;
-            System.out.println("player is in jail");
-            assertTrue(playerInJail);
-        }
+        assertFalse("dont have getoutofjail card", player.getJailCardStatus());
 
+        drawcontroller.draw(card);
+
+        assertTrue("have getoutofjail card", player.getJailCardStatus());
     }
 
-     */
-
-
     @Test
-    public void MonopolyJackpotCard() {
+    public void MonopolyJackpotCard () {
 
-        MonopolyJackpotCard card = new MonopolyJackpotCard("desc",750,2000);
+        MonopolyJackpotCard card = new MonopolyJackpotCard("desc", 750, 2000);
 
-        assertEquals(1500,player.getBalance());
+        assertEquals(1500, player.getBalance());
 
         player.addToBalance(-1000);
         drawcontroller.draw(card);
 
-        assertEquals(2500,player.getBalance());
-
-        //String message = card.getDescription();
-        //viewController.showMessage(message);
-
-        //int jackpot = card.getJackpot();
-        //int amount = card.getAmount();
-
-
-
-        //assertEquals(1500,player.getBalance());
-
-
+        assertEquals(2500, player.getBalance());
     }
 
 
@@ -113,13 +88,77 @@ public class DrawControllerTest {
     public void PayForBuildingsCard() {
     }
 
+*/
+
     @Test
     public void TeleportAndPayDoubleCard() {
+
+        TeleportAndPayDoubleCard card = new TeleportAndPayDoubleCard("desc", 2);
+
+
+        drawcontroller.draw(card);
+/*
+        assertEquals();
+
+        int amount = 25*card.getMultiplier();
+
+        int oldPosition = player.getPosition();
+
+        int firFerry = 5;
+        int secFerry = 15;
+        int thiFerry = 25;
+        int fouFerry = 35;
+
+        if (player.getPosition() < 5 || player.getPosition() > 35) {
+            player.setPosition(firFerry);
+        } else if (player.getPosition() > 5 && player.getPosition() < 15) {
+            player.setPosition(secFerry);
+        } else if (player.getPosition() > 15 && player.getPosition() < 25) {
+            player.setPosition(thiFerry);
+        } else if (player.getPosition() > 25 && player.getPosition() < 35) {
+            player.setPosition(fouFerry);
+        }
+
+        int newPosition = player.getPosition();
+
+        String positionAsString = String.valueOf(newPosition);
+        Field disbutedField = bank.getFieldById(positionAsString);
+
+        viewController.teleportPlayer(player.getName(), oldPosition, newPosition);
+
+        if(bank.hasOwner(disbutedField.getID())){
+            Player otherPlayer =  bank.getOwner(positionAsString);
+            tradeController.transferAssets(player, otherPlayer, amount);
+        } else{
+            tradeController.askIfWantToBuy(player, disbutedField);
+        }
+
+        //først at den er teleported
+
+        //så om den er ejet
+
+        //betaling
+
+        //købning
+
+    }
+
+*/
     }
 
     @Test
     public void GoToJail() {
+
+    GoToJail card = new GoToJail("desc",10);
+
+    assertFalse("is not in jail", player.isInJail());
+
+    drawcontroller.draw(card);
+
+    assertTrue("Player is in jail",player.isInJail());
     }
+
+    /*
 
     @Test
     public void BirthdayCard() {
@@ -136,4 +175,4 @@ public class DrawControllerTest {
 
     */
 
-}
+    }
